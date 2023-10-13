d1884d892c0fcf951d83178eec886803fea11e16
viet dong <levietdongqt@gmail.com>
2023-10-13 17:39:31 +0700

    update

--- WebAPIGroup2/Respository/Implement/MyImageRepo.cs
+++ WebAPIGroup2/Respository/Implement/MyImageRepo.cs
@@ -20,7 +20,7 @@ namespace WebAPIGroup2.Respository.Implement
         {
             try
             {
-                var list = await _context.MyImages.Where(t => t.PurchaseOrderId == purchaseOrderId).ToListAsync();
+                var list = await _context.MyImages.Include(t=> t.PurchaseOrder).Include(t=>t.Images).Where(t => t.PurchaseOrderId == purchaseOrderId).ToListAsync();
                 if(list.Count == 0)
                 {
                     return null;
@@ -30,6 +30,62 @@ namespace WebAPIGroup2.Respository.Implement
             catch (Exception e )
             {
                 Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public async Task<List<MyImage>> getByUserId(int userID)
+        {
+            try
+            {
+                var listPurchase = await _context.PurchaseOrders.Include(t => t.MyImages).ThenInclude(t=> t.Images).Where(t => t.UserId == userID && (t.Status == PurchaseStatus.Temporary || t.Status == PurchaseStatus.InCart)).ToListAsync();
+                if(listPurchase.Count == 0)
+                {
+                    return null;
+                }
+                int[] purchaseIDs = new int[listPurchase.Count];
+                listPurchase.ForEach(purchase =>
+                {
+                    purchaseIDs[0] = purchase.Id;
+                });
+                List<MyImage> list = await _context.MyImages.Include(t=> t.Images).Include(t=> t.Template).ThenInclude(t=> t.TemplateSizes).ThenInclude(t=> t.PrintSize).Where(t=> purchaseIDs.Contains(t.Id)).ToListAsync();
+                if(list.Count == 0)
+                {
+                    return null;
+                }
+                return list;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public async Task<List<MyImage>> loadInCart(int userID)
+        {
+            try
+            {
+                var puchase = await _context.PurchaseOrders.Include(t => t.MyImages).FirstOrDefaultAsync(t => t.UserId == userID && t.Status == PurchaseStatus.InCart);
+                if (puchase == null)
+                {
+                    return null;
+                }
+                var myImages = await _context.MyImages.Include(t=> t.Images)
+                    .Include(t=> t.ProductDetails)
+                        .ThenInclude(t=> t.MaterialPage)
+                    .Include(t=> t.ProductDetails)
+                        .ThenInclude(pd=> pd.TemplateSize).ThenInclude(ps=> ps.PrintSize)
+                    .Where(t => t.PurchaseOrderId == puchase.Id).ToListAsync();
+                if(myImages.Count == 0)
+                {
+                    return null;
+                }
+                return myImages;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
                 return null;
             }
         }
--- WebAPIGroup2/Respository/Implement/ProductDetailRepo.cs
+++ WebAPIGroup2/Respository/Implement/ProductDetailRepo.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using NuGet.Packaging.Core;
 using WebAPIGroup2.Models;
+using WebAPIGroup2.Models.DTO;
 using WebAPIGroup2.Models.POJO;
 using WebAPIGroup2.Respository.Inteface;
 
@@ -13,11 +14,25 @@ namespace WebAPIGroup2.Respository.Implement
         {
         }
 
-        public Task<ProductDetail?> GetByIDAsync(int id)
+        public async Task<ProductDetail?> GetByIDAsync(int id)
         {
             try
             {
-               return _context.ProductDetails.FirstOrDefaultAsync(x => x.Id == id);
+               return await _context.ProductDetails.FirstOrDefaultAsync(x => x.Id == id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public async Task<ProductDetail?> GetByMyImageId(OrderDTO orderDTO)
+        {
+            try
+            {
+                var product = await _context.ProductDetails.FirstOrDefaultAsync(t => t.MyImageId == orderDTO.myImageID && t.MaterialPageId == orderDTO.materialPageId && t.TemplateSizeId == orderDTO.temlateSizeId);
+                return product;
             }
             catch (Exception e)
             {
